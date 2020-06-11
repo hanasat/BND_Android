9dc0abcea131188106027b7f0eef659c41d6de45
hanasat <58374123+hanasat@users.noreply.github.com>
2020-06-11 12:01:29 +0200

    l'ajout de la fonctionnalité : déconnexion

--- app/src/main/java/com/hsati/bndproject/HomeClientActivity.java
+++ app/src/main/java/com/hsati/bndproject/HomeClientActivity.java
@@ -1,15 +1,21 @@
 package com.hsati.bndproject;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class HomeClientActivity extends AppCompatActivity {
 
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_client);
@@ -49,5 +55,10 @@ public class HomeClientActivity extends AppCompatActivity {
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void SingOut(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(this, ConnectionActivity.class));
     }
 }
